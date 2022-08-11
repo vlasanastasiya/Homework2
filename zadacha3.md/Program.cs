43969c130f475593679b5619902ea3d28cba5283
vlasenkova <asyicheva1@mail.ru>
2022-08-11 20:13:27 +0300

    дополнила задачу

--- zadacha3.md/Program.cs
+++ zadacha3.md/Program.cs
@@ -5,22 +5,22 @@
 // 1 -> нет
 Random rand = new Random();
 int number = rand.Next(1, 8);
-Console.WriteLine(number);
+Zadacha3(number);
 
 void Zadacha3 (int currentnumber) {
 Console.WriteLine(currentnumber);
     if (currentnumber == 1)
-        Console.WriteLine("Сегодня понедельник - рабочий день");
+        Console.WriteLine("Это день понедельник - рабочий день");
     if (currentnumber == 2)
-        Console.WriteLine("Сегодня вторник - рабочий день");
+        Console.WriteLine("Это день вторник - рабочий день");
     if (currentnumber == 3)
-        Console.WriteLine("Сегодня среда - рабочий день");
+        Console.WriteLine("Это день среда - рабочий день");
     if (currentnumber == 4)
-    Console.WriteLine("Сегодня четверг - рабочий день");
+    Console.WriteLine("Это день четверг - рабочий день");
     if (currentnumber == 5)
-    Console.WriteLine("Сегодня пятница - рабочий день");
+    Console.WriteLine("Это день пятница - рабочий день");
     if (currentnumber == 6)
-    Console.WriteLine("Сегодня суббота - выходной день");
+    Console.WriteLine("Это день суббота - выходной день");
     if (currentnumber == 7)
-    Console.WriteLine("Сегодня воскресенье - выходной день");
+    Console.WriteLine("Это день воскресенье - выходной день");
 }
