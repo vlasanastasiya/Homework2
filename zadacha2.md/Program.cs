a42d2e323d49830fe33d4ba8e831bcbd41d9536f
vlasenkova <asyicheva1@mail.ru>
2022-08-11 17:17:12 +0300

    вторая задача

---
+++ zadacha2.md/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет3
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+Zadacha2(number);
+
+void Zadacha2(string currentnumber)
+{
+    if (currentnumber.Length >= 3)
+    {
+        Console.WriteLine($"Третья цифра числа {number} равна {currentnumber[2]}");
+    }
+    else {
+        Console.WriteLine("третьей цифры нет");
+    }
+}
