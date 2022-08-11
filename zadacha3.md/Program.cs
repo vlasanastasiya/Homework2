cf9fc0dbd9e95550d4b1e1f00b9fc3da713340cc
vlasenkova <asyicheva1@mail.ru>
2022-08-11 20:10:08 +0300

    добавила дни недели

--- zadacha3.md/Program.cs
+++ zadacha3.md/Program.cs
@@ -7,15 +7,20 @@ Random rand = new Random();
 int number = rand.Next(1, 8);
 Console.WriteLine(number);
 
-void Zadacha3 (int currentnumber) 
-{
-  if (currentnumber == 6 || currentnumber == 7) 
-  {
-       Console.WriteLine("Это выходной день");
-  }
-  else 
-  {
-       Console.WriteLine("Это рабочий день");
-  }
+void Zadacha3 (int currentnumber) {
+Console.WriteLine(currentnumber);
+    if (currentnumber == 1)
+        Console.WriteLine("Сегодня понедельник - рабочий день");
+    if (currentnumber == 2)
+        Console.WriteLine("Сегодня вторник - рабочий день");
+    if (currentnumber == 3)
+        Console.WriteLine("Сегодня среда - рабочий день");
+    if (currentnumber == 4)
+    Console.WriteLine("Сегодня четверг - рабочий день");
+    if (currentnumber == 5)
+    Console.WriteLine("Сегодня пятница - рабочий день");
+    if (currentnumber == 6)
+    Console.WriteLine("Сегодня суббота - выходной день");
+    if (currentnumber == 7)
+    Console.WriteLine("Сегодня воскресенье - выходной день");
 }
-Zadacha3(number);
